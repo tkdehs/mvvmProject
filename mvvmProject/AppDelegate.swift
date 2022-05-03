0c6733d5edc5b6fe53b79e9878d18e23d61bd2a9
PNX <tkdehs0454@naver.com>
2022-05-03 11:48:54 +0900

    firstCommit

--- mvvmProject/AppDelegate.swift
+++ mvvmProject/AppDelegate.swift
@@ -10,26 +10,19 @@ import CoreData
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+    
+    var window: UIWindow? = UIWindow(frame: UIScreen.main.bounds)
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        self.setMainView()
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    
+    func setMainView(){
+        let nvMain = UINavigationController(rootViewController: MainViewController())
+        self.window?.rootViewController = nvMain
+        self.window?.makeKeyAndVisible()
     }
 
     // MARK: - Core Data stack
